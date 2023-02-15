9239a6bf0812a89f62d654c5e74d03ae928b3480
Ciman Code <firmanabdul278@gmail.com>
2023-02-15 09:26:11 +0700

    (+) user profile

---
+++ NgajiLagi/View/UserProfile.xaml.cs
@@ -0,0 +1,9 @@
+namespace NgajiLagi.View;
+
+public partial class UserProfile : ContentPage
+{
+	public UserProfile()
+	{
+		InitializeComponent();
+	}
+}
--- NgajiLagi/obj/Debug/net6.0-android/android/environment.armeabi-v7a.s
+++ NgajiLagi/obj/Debug/net6.0-android/android/environment.armeabi-v7a.s
@@ -3332,7 +3332,7 @@ application_config:
 
 	.type	.L.autostr.8, %object
 .L.autostr.8:
-	.asciz	"0eb699f1-f7ad-4b90-b130-a9e1130dd05c"
+	.asciz	"30709bce-0e8f-4bed-8c0a-53e6f4ceedca"
 	.size	.L.autostr.8, 37
 
 	.type	.L.autostr.9, %object
